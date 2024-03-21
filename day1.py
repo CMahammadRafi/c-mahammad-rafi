d07c0e1cfd000879c81b885c7e1a6435cd05277d
CMahammadRafi <162982189+CMahammadRafi@users.noreply.github.com>
2024-03-21 11:18:00 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,60 @@
+0'''
+                         BARGAINED CHARACTERS
+
+
+Contorls and data structure - ABC language
+string and its formats - from c
+regular expressions - pearl
+exception handling - module 3
+magic methods form - fortron
+
+what kind of language is python?
+
+python is a
+
+    object oriented programing language
+    dynamically typed language
+    scripting language
+    interepted language
+    compiled language
+
+what are the flavours of python
+
+>cpython- written in c, most common implementation of python
+>jython - java implementation of python
+>ironpython- implementation of python programming language which is tightly integrated with NET.
+>brytho- browser python, runs in browsers
+>rubypython- bridge between python and ruby interpreters
+>micropython- runs in micro controllers
+
+#---->  print(object, sep, end, file, flush)
+# print has 5 parameters
+
+# object ----> the value to be printed is object
+# sep---> seperator
+# Eg
+# a=8
+# b=9
+
+# c=89
+# print(a, b,c, sep='$')
+
+# end--> which is used to add string at the end of the value,
+## it will not keep the command to stay in same line
+#Eg:
+#print(str(a), b, end='%%')
+#print(str(c))
+
+#!---> variables
+# ?variables is a name which is used to ideentify thee value of variable
+# ?which is stored in a specific memory location
+
+#! rules to define variables
+#1.) variable have to start with only alphabets, underscores
+#2.) the char following the 1st char can be alphabets, numbers, underscores
+#3.) no spaces are allowed
+#4.) case sensitivity
+#6.) keywords are not allowed as variables
+'''
+#!
+ 
